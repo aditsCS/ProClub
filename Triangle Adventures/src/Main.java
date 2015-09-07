f08bdc03cc4e62cbeead30d3208d3f741dae5c40
Aditya <adityasampath@gmail.com>
2015-09-07 01:25:43 -0700

    CardLayout and new screens

--- Triangle Adventures/src/Main.java
+++ Triangle Adventures/src/Main.java
@@ -1,30 +1,47 @@
+import java.awt.CardLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
 
 	public static final int STEP = 500;
 
-	public static void main(String[] args) {
+	private JFrame frame;
+	private JPanel cardPanel;
+
+	public Main () {}
+
+	public void run () {
 		final World world = new World();
 		final Player player = new Player();
 		final Gui gui = new Gui(world);
 
 		// change the level number to change levels
-		final Level level = new Level9();
+		final Level level = new Level1();
 		level.load(world, player);
 
-		showGui(gui);
+		frame = showGui(gui);
 		startLoop(world, gui);
 
+		commandPlayer(player);
+	}
+
+	public void commandPlayer (Player player) {
 		// Player commands go here
 		player.move();
 		player.attack();
 	}
 
 	@SuppressWarnings("serial")
-	public static JFrame showGui(final Gui gui) {
+	public JFrame showGui(final Gui gui) {
 		return new JFrame("Triangle Adventures") {{
-			add(gui);
+		    cardPanel = new JPanel();
+		    CardLayout cl = new CardLayout();
+		    cardPanel.setLayout(cl);
+
+		    cardPanel.add(gui, "1");
+
+			add(cardPanel);
 			pack();
 			setResizable(false);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,7 +49,12 @@ public class Main {
 		}};
 	}
 
-	public static void startLoop(final World world, final Gui gui) {
+	public void changePanel(String panel) {
+		((CardLayout)cardPanel.getLayout()).next(cardPanel);
+		frame.requestFocus();
+	}
+
+	public void startLoop(final World world, final Gui gui) {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -49,4 +71,8 @@ public class Main {
 		}).start();
 	}
 
+	public static void main(String[] args) {
+		final Main m = new Main();
+		m.run();
+	}
 }
