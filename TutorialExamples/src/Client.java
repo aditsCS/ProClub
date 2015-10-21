1b77c5020d39ad9672eca481af49c3d9cbea3baa
Heidi Dong <dong.heidi@gmail.com>
2015-10-21 00:00:43 -0700

    change streams to use BufferedReader and PrintWriter

--- TutorialExamples/src/Client.java
+++ TutorialExamples/src/Client.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -11,20 +14,20 @@ public class Client {
 	public static void main(String[] args) {
 		String host = "localhost";
 		int port = 8000;
-		DataInputStream in;
-		DataOutputStream out;
+		BufferedReader in;
+		PrintWriter out;
 		Socket socket;
 
 		Scanner input = new Scanner(System.in);
 
 		try {
 			socket = new Socket(host, port);
-			in = new DataInputStream(socket.getInputStream());
-			out = new DataOutputStream(socket.getOutputStream());
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
 			while (true) {
-				System.out.print("Send number to server: ");
-				out.writeInt(input.nextInt());
-				JOptionPane.showMessageDialog(null, "Server sent number: "+in.readInt());
+				System.out.print("Text to send: ");
+				out.println(input.nextLine());
+				JOptionPane.showMessageDialog(null, "Server says: "+in.readLine());
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
--- TutorialExamples/src/Server.java
+++ TutorialExamples/src/Server.java
@@ -1,6 +1,7 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,20 +11,20 @@ public class Server {
 
 	public static void main(String[] args) {
 		int port = 8000;
-		DataInputStream in;
-		DataOutputStream out;
+		BufferedReader in;
+		PrintWriter out;
 		ServerSocket server;
 		Socket socket;
 
 		try {
 			server = new ServerSocket(port);
 			socket = server.accept();
-			in = new DataInputStream(socket.getInputStream());
-			out = new DataOutputStream(socket.getOutputStream());
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
 			while (true) {
-				int x = in.readInt();
-				JOptionPane.showMessageDialog(null, "Number recieved from client: " + x);
-				out.writeInt(x*2);
+				String text = in.readLine();
+				JOptionPane.showMessageDialog(null, "Text recieved from client: " + text);
+				out.println("Your message was "+text.length()+" characters long.");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
